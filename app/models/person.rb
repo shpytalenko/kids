4fc60e434613b0d4b05bbfa51581c835b7986de4
shpytalenko <shpytalenko@gmail.com>
2014-04-10 00:54:44 +0400

    update

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,13 +1,11 @@
 class Person < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :sex, :dob, :age, :school_number, :school_type, :klass, :headshot_photo_id
   #belongs_to :headshot_photo
-  has_many :headshot_photos, :as => :capturable
+  has_many :headshot_photos, :as => :capturable, :dependent => :destroy
   has_and_belongs_to_many :variants
- # after_create :add_photo
+  after_create :clear
 
-  def add_photo
-    if HeadshotPhoto.all.size > 0
-      self.headshot_photos << HeadshotPhoto.all.last
-    end
+  def clear
+    Person.where(["created_at < ?", 2.days.ago]).delete_all 
   end
 end
